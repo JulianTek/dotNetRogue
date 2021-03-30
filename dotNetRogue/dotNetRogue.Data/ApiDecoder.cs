fa1076a85fe44cbb9a1f1f107af176ddecaba274
Julian <Julian-Tekstra@hotmail.com>
2021-03-30 14:40:13 +0200

    Setup beginning of API decoder

---
+++ dotNetRogue/dotNetRogue.Data/ApiDecoder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using dotNetRogue.Data.Models;
+using Newtonsoft.Json;
+
+namespace dotNetRogue.Data
+{
+    public class ApiDecoder
+    {
+
+        // localhost url: https://localhost:44328
+
+        public async Task<WeaponDTO> GenerateWeaponDto()
+        {
+            string url = "https://localhost:44328/weapon";
+            using (HttpResponseMessage response = await APICaller.ApiClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var weaponDto = await response.Content.ReadFromJsonAsync<WeaponDTO>();
+                    return weaponDto;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
---
+++ dotNetRogue/dotNetRogue.Data/Models/WeaponDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNetRogue.Data.Models
+{
+    public class WeaponDTO
+    {
+        public WeaponDTO(string name, WeaponTypeDTO type, Dictionary<string, int> stats, WeaponRarityDTO rarity, List<EffectDTO> effects, int sellValue)
+        {
+            Name = name;
+            Type = type;
+            Stats = stats;
+            Rarity = rarity;
+            Effects = effects;
+            SellValue = sellValue;
+        }
+
+        public string Name { get; private set; }
+        public WeaponTypeDTO Type { get; private set; }
+        public Dictionary<string, int> Stats { get; private set; }
+        public WeaponRarityDTO Rarity { get; private set; }
+        public List<EffectDTO> Effects { get; private set; }
+        public int SellValue { get; private set; }
+    }
+}
