9d7beec415b26c0a861947e6e6beaae526e4e620
Julian <Julian-Tekstra@hotmail.com>
2021-04-06 15:31:50 +0200

    Start implementation adding enemy types

--- dotNetRogue/dotNetRogue/ClientApp/src/components/Enemy.vue
+++ dotNetRogue/dotNetRogue/ClientApp/src/components/Enemy.vue
@@ -1,10 +1,21 @@
 ﻿using dotNetRogue.Logic.Models;
+<style type="text/css">
+    #overlay {
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background: rgba(0,0,0, 0.5);
+    }
+</style>
 <template>
     <h1>Enemy types:</h1>
     <table class='table table-striped' aria-labelledby="tableLabel" v-if="enemyTypes">
         <thead>
             <tr>
                 <th>Name</th>
+                <th>Attack</th>
                 <th>Health</th>
                 <th>Defense</th>
                 <th>Speed</th>
@@ -14,13 +25,39 @@
         <tbody>
             <tr v-for="enemy of enemyTypes" v-bind:key="enemy.name">
                 <td>{{ enemy.name }}</td>
-                <td>{{enemy.health}}</td>
-                <td>{{enemy.defense}}</td>
-                <td>{{enemy.speed}}</td>
-                <td>{{enemy.goldOnKill}}</td>
+                <td>{{ enemy.health }}</td>
+                <td>{{ enemy.attack }}</td>
+                <td>{{ enemy.defense }}</td>
+                <td>{{ enemy.speed }}</td>
+                <td>{{ enemy.goldOnKill }}</td>
             </tr>
         </tbody>
     </table>
+
+    <!--Adding enemy type-->
+    <div id="overlay" v-if="showOverlay">
+        <div class="modal-dialogue">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Add new enemy type</h5>
+                    <button type="button" class="close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <input type="text" name="name" class="form-control form-control-lg" placeholder="Name" />
+                    <input type="text" name="health" class="form-control form-control-lg" placeholder="Heatlh" />
+                    <input type="text" name="attack" class="form-control form-control-lg" placeholder="Attack" />
+                    <input type="text" name="defense" class="form-control form-control-lg" placeholder="Defense" />
+                    <input type="text" name="speed" class="form-control form-control-lg" placeholder="Speed" />
+                    <input type="text" name="goldOnKill" class="form-control form-control-lg" placeholder="Gold on kill" />
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-info btn-block btn-lg">>Add enemy type</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -29,11 +66,13 @@
         name: "Game",
         data() {
             return {
-                enemyTypes: []
+                enemyTypes: [],
+                showOverlay:
+                    false,
             }
         },
         methods: {
-            getAll() {
+    getAll() {
                 axios.get('/enemy')
                     .then((response) => {
                         this.enemyTypes = response.data;
