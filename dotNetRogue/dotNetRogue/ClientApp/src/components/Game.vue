7db49b974b7bb218078f8a4de1d01d98edcd8784
Julian <Julian-Tekstra@hotmail.com>
2021-04-14 14:24:53 +0200

    Add messagebox

--- dotNetRogue/dotNetRogue/ClientApp/src/components/Game.vue
+++ dotNetRogue/dotNetRogue/ClientApp/src/components/Game.vue
@@ -1,14 +1,15 @@
-﻿<template>
+﻿using System;
+using dotNetRogue.Logic.Models;
+<template>
     <h1>dotNetRogue</h1>
-
-    <p v-if="enemy != null">{{enemy.name}} attacks you!</p>
-
-    <p aria-live="polite" v-if="enemy != null">Current enemy health: <strong>{{ enemy.health }}</strong></p>
+    <p v-if="enemy != null">You are attacked by a {{enemy.name}}</p>
     <p>Your health is: <strong>{{playerHealth}}</strong></p>
+
     <p v-if="weapon != null">You have a {{weapon.name}}</p>
     <button v-if="weapon != null && enemy != null && canAttack" class="btn btn-primary" @click="attack">Attack!</button>
     <button v-if="!canAttack" class="btn btn-primary" @click="block(enemyAttack())">Block!</button>
     <button v-if="!canAttack" class="btn btn-primary" @click="dodge(enemyAttack())">Dodge!</button>
+    <p aria-live="polite"> {{ gameMessage }}</p>
 </template>
 
 
@@ -22,11 +23,13 @@
                 canAttack: true,
                 weapon: null,
                 enemy: null,
+                gameMessage: "",
             }
         },
         methods: {
             attack() {
                 var dmg = Math.floor(Math.random() * this.weapon.stats["Attack"]) + Math.round(this.weapon.stats["Attack"] / 2);
+                this.gameMessage = "Dealt " + dmg + " damage";
                 this.enemy.health = this.enemy.health - dmg;
                 this.canAttack = false;
             },
@@ -38,10 +41,11 @@
                 var newDodgeChance = this.weapon.stats["Dodge"] + Math.floor(Math.random() * 10) + 1;
                 var nr = Math.floor(Math.random() * 100) + 1;
                 if (nr < newDodgeChance) {
-                    // Dodge is succesful
+                    this.gameMessage = "Dodge successful!";
                 }
                 else {
                     this.playerHealth = this.playerHealth - enemyDmg;
+                    this.gameMessage = "Dodge failed! Enemy dealt " + enemyDmg + " damage!";
                 }
                 this.canAttack = true;
             },
