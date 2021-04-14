4dd39daaf4c68756b0554efd10c69248d73acc67
Julian <Julian-Tekstra@hotmail.com>
2021-04-14 15:02:34 +0200

    Add enemy editing overlay

--- dotNetRogue/dotNetRogue/ClientApp/src/components/Enemy.vue
+++ dotNetRogue/dotNetRogue/ClientApp/src/components/Enemy.vue
@@ -15,6 +15,7 @@
     <table class='table table-striped' aria-labelledby="tableLabel" v-if="enemyTypes">
         <thead>
             <tr>
+                <th></th>
                 <th>Name</th>
                 <th>Attack</th>
                 <th>Health</th>
@@ -25,6 +26,7 @@
         </thead>
         <tbody>
             <tr v-for="enemy of enemyTypes" v-bind:key="enemy.name">
+                <button @click="editEnemyOverlay">Edit</button>
                 <td>{{ enemy.name }}</td>
                 <td>{{ enemy.health }}</td>
                 <td>{{ enemy.attack }}</td>
@@ -36,17 +38,42 @@
     </table>
 
     <!--Adding enemy type-->
-    <div id="overlay" v-if="showOverlay">
+    <div id="overlay" v-if="showAddOverlay">
         <div class="modal-dialogue">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title">Add new enemy type</h5>
                     <button type="button" class="close">
-                        <span aria-hidden="true" @click="hideEnemyOverlay">&times;</span>
+                        <span aria-hidden="true" @click="hideAddEnemyOverlay">&times;</span>
                     </button>
                 </div>
                 <form class="modal-body">
                     <input type="text" name="name" v-model="this.name" class="form-control form-control-lg" placeholder="Name" />
+                    <input type="number" name="health" v-model="this.health" class="form-control form-control-lg" placeholder="Heatlh" />
+                    <input type="number" name="attack" v-model="this.attack" class="form-control form-control-lg" placeholder="Attack" />
+                    <input type="number" name="defense" v-model="this.defense" class="form-control form-control-lg" placeholder="Defense" />
+                    <input type="number" name="speed" v-model="this.speed" class="form-control form-control-lg" placeholder="Speed" />
+                    <input type="number" name="goldOnKill" v-model="goldOnKill" class="form-control form-control-lg" placeholder="Gold on kill" />
+                </form>
+                <div class="form-group">
+                    <button class="btn btn-info btn-block btn-lg" @click="addEnemy">Add enemy type</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--Editing data type-->
+    <div id="overlay" v-if="showEditOverlay">
+        <div class="modal-dialogue">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Edit enemy type</h5>
+                    <button type="button" class="close">
+                        <span aria-hidden="true" @click="hideEditEnemyOverlay">&times;</span>
+                    </button>
+                </div>
+                <form class="modal-body">
+                    <input type="text" name="name" v-model="this.name" class="form-control form-control-lg" disabled placeholder="Name" />
                     <input type="number" name="health" v-model="this.health" class="form-control form-control-lg" placeholder="Heatlh" />
                     <input type="number" name="attack" v-model="this.attack" class="form-control form-control-lg" placeholder="Attack" />
                     <input type="number" name="defense" v-model="this.defense" class="form-control form-control-lg" placeholder="Defense" />
@@ -68,7 +95,8 @@
         data() {
             return {
                 enemyTypes: [],
-                showOverlay: false,
+                showAddOverlay: false,
+                showEditOverlay: false,
                 name: "",
                 health: 0,
                 attack: 0,
@@ -88,12 +116,18 @@
                     });
             },
             addEnemyOverlay() {
-                this.showOverlay = true;
+                this.showAddOverlay = true;
             },
-            hideEnemyOverlay() {
-                this.showOverlay = false;
+            editEnemyOverlay() {
+                this.showEditOverlay = true;
             },
-            async       addEnemy() {
+            hideAddEnemyOverlay() {
+                this.showAddOverlay = false;
+            },
+            hideEditEnemyOverlay() {
+                this.showEditOverlay = false;
+            },
+            async addEnemy() {
                 var enemy = {
                     name: this.name,
                     health: this.health,
@@ -103,14 +137,14 @@
                     goldOnKill: this.goldOnKill
                 }
                 console.log(enemy);
-                await axios.post('/enemy', {name: this.name, health: this.health, attack: this.attack, defense: this.defense, speed: this.speed, goldOnKill: this.goldOnKill}).then((response) => {
+                await axios.post('/enemy', { name: this.name, health: this.health, attack: this.attack, defense: this.defense, speed: this.speed, goldOnKill: this.goldOnKill }).then((response) => {
                     console.log(response);
                 })
                     .catch(function (error) {
                         alert(error.message);
                     });
                 this.enemyTypes.push(enemy);
-                this.showOverlay = false;   
+                this.showOverlay = false;
             },
         },
         mounted() {
