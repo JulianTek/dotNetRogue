193f7c6e9b9272c180caec8e644a7aef64a91a22
Julian <Julian-Tekstra@hotmail.com>
2021-06-03 15:06:03 +0200

    Second frontend design
    Proper use of backgrounds and small UI

--- dotNetRogue/dotNetRogue/ClientApp/src/components/Game.vue
+++ dotNetRogue/dotNetRogue/ClientApp/src/components/Game.vue
@@ -1,25 +1,28 @@
 ﻿using System;
 using dotNetRogue.Logic.Models;
 <template>
-    <div style="background-color: black">
-        <h1>dotNetRogue</h1>
-        <p v-if="enemy != null">You are attacked by a {{ enemy.name }}</p>
-        <p>Your health is: <strong>{{ playerHealth }}</strong></p>
-        <img :src="image" />
-        <div style="margin: auto;">
-            <img src="../assets/bg.png" />
-        </div>
+    <div style="background-image: url('https://i.redd.it/6axfm8uck3oy.png'); height: 100vh;">
+        <img src="../assets//skeleton.png" style="width: 150px; height: 400px;" v-if="enemy != null" />
 
         <p v-if="weapon != null">You have a {{ weapon.name }}</p>
         <div v-if="showButtons">
             <button v-if="weapon != null && enemy != null && canAttack" class="btn btn-primary" @click="attack">Attack!</button>
             <button v-if="!canAttack" class="btn btn-primary" @click="block(enemyAttack())">Block!</button>
             <button v-if="!canAttack" class="btn btn-primary" @click="dodge(enemyAttack())">Dodge!</button>
-            <p>You have {{ playerGold }} gold</p>
         </div>
-        <p aria-live="polite"> {{ gameMessage }}</p>
-        <p aria-live="polite" v-if="showGoldMsg">Gained {{ enemy.goldOnKill }} gold</p>
-
+        <div style="background-color: dimgray; border: 5px solid black; height: 100px; margin-left: 10px; margin-right: 10px;" v-if="enemy != null">
+            <p strong v-if="enemy != null" style="color: white;">You are attacked by a {{ enemy.name }}</p>
+            <p strong aria-live="polite" style="color: white;"> {{ gameMessage }}</p>
+            <p strong aria-live="polite" v-if="showGoldMsg" style="color: white;">Gained {{ enemy.goldOnKill }} gold</p>
+        </div>
+        <div v-if="enemy != null" style="background-color: black; margin-left: 10px; margin-right: 10px;">
+            <div style="float: left;">
+                <img src="../assets/life.png" style="height: 50px; width: 50px; float: left;" /> <p style="color: red">{{ playerHealth }}/100</p>
+            </div>
+            <div style="margin: auto;">
+                <img src="../assets/gold.png" style="height: 40px; width: 40px" /> <p style="color: gold">{{ playerGold }}</p>
+            </div>
+        </div>
         <!--Overlay of loot dialog-->
         <div id="overlay" v-if="lootOverlay">
             <div class="modal-dialogue">
@@ -108,7 +111,7 @@ using dotNetRogue.Logic.Models;
                 return dmg;
             },
             async generateLoot() {
-               await axios.get('/weapongenerator')
+                await axios.get('/weapongenerator')
                     .then((response) => {
                         this.newWeapon = response.data;
                     })
